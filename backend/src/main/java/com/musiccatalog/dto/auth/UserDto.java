2350aabd8b4448c306294186b7a838fc55174ad3
Developer <dev@musiccatalog.local>
2026-07-30 20:00:40 +0530

    feat(auth): add UserDto user response model

---
+++ backend/src/main/java/com/musiccatalog/dto/auth/UserDto.java
@@ -0,0 +1,16 @@
+package com.musiccatalog.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+    private String email;
+    private String fullName;
+}
